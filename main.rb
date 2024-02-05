a0186301bc07d6128b2c2f61e50de56012d9f620
Nicklas <95386272+Nick-ThP@users.noreply.github.com>
2024-02-05 23:07:01 +0100

    almost configured

---
+++ main.rb
@@ -0,0 +1,13 @@
+module SimpleMath
+  def self.add(x, y)
+    x + y
+  end
+
+  def self.subtract(x, y)
+    x - y
+  end
+end
+
+@value = SimpleMath.add(1, 2)
+@value2 = SimpleMath.subtract(5, 2)
+put hallo
